1dd5582085d77608593414192df6a576b4596e7a
Saúl Ibarra Corretgé <saghul@gmail.com>
2015-03-06 09:31:23 +0100

    win: fix uv_pipe_getpeername for client pipes

--- src/win/pipe.c
+++ src/win/pipe.c
@@ -1915,68 +1915,81 @@ int uv_pipe_open(uv_pipe_t* pipe, uv_file file) {
 
 
 static int uv__pipe_getname(const uv_pipe_t* handle, char* buffer, size_t* size) {
-  NTSTATUS nt_status;
-  IO_STATUS_BLOCK io_status;
-  FILE_NAME_INFORMATION tmp_name_info;
-  FILE_NAME_INFORMATION* name_info;
   WCHAR* name_buf;
   unsigned int addrlen;
   unsigned int name_size;
   unsigned int name_len;
-  int err;
-
-  name_info = NULL;
 
   if (handle->handle == INVALID_HANDLE_VALUE) {
     *size = 0;
     return UV_EINVAL;
   }
 
-  uv__pipe_pause_read((uv_pipe_t*)handle); /* cast away const warning */
+  if (!(handle->flags & UV_HANDLE_CONNECTION)) {
+    NTSTATUS nt_status;
+    IO_STATUS_BLOCK io_status;
+    FILE_NAME_INFORMATION tmp_name_info;
+    FILE_NAME_INFORMATION* name_info;
 
-  nt_status = pNtQueryInformationFile(handle->handle,
-                                      &io_status,
-                                      &tmp_name_info,
-                                      sizeof tmp_name_info,
-                                      FileNameInformation);
-  if (nt_status == STATUS_BUFFER_OVERFLOW) {
-    name_size = sizeof(*name_info) + tmp_name_info.FileNameLength;
-    name_info = uv__malloc(name_size);
-    if (!name_info) {
-      *size = 0;
-      err = UV_ENOMEM;
-      goto cleanup;
-    }
+    name_info = NULL;
+
+    uv__pipe_pause_read((uv_pipe_t*)handle); /* cast away const warning */
 
     nt_status = pNtQueryInformationFile(handle->handle,
                                         &io_status,
-                                        name_info,
-                                        name_size,
+                                        &tmp_name_info,
+                                        sizeof tmp_name_info,
                                         FileNameInformation);
-  }
+    if (nt_status == STATUS_BUFFER_OVERFLOW) {
+      name_size = sizeof(*name_info) + tmp_name_info.FileNameLength;
+      name_info = uv__malloc(name_size);
+      if (!name_info) {
+        *size = 0;
+        uv__pipe_unpause_read((uv_pipe_t*)handle); /* cast away const warning */
+        return UV_ENOMEM;
+      }
 
-  if (nt_status != STATUS_SUCCESS) {
-    *size = 0;
-    err = uv_translate_sys_error(pRtlNtStatusToDosError(nt_status));
-    goto error;
-  }
+      nt_status = pNtQueryInformationFile(handle->handle,
+                                          &io_status,
+                                          name_info,
+                                          name_size,
+                                          FileNameInformation);
+    }
 
-  if (!name_info) {
-    /* the struct on stack was used */
-    name_buf = tmp_name_info.FileName;
-    name_len = tmp_name_info.FileNameLength;
+    if (nt_status != STATUS_SUCCESS) {
+      int err;
+      *size = 0;
+      err = uv_translate_sys_error(pRtlNtStatusToDosError(nt_status));
+      uv__free(name_info);
+      uv__pipe_unpause_read((uv_pipe_t*)handle); /* cast away const warning */
+      return err;
+    }
+
+    if (!name_info) {
+      /* the struct on stack was used */
+      name_buf = tmp_name_info.FileName;
+      name_len = tmp_name_info.FileNameLength;
+    } else {
+      name_buf = name_info->FileName;
+      name_len = name_info->FileNameLength;
+    }
+
+    if (name_len == 0) {
+      *size = 0;
+      uv__free(name_info);
+      uv__pipe_unpause_read((uv_pipe_t*)handle); /* cast away const warning */
+      return 0;
+    }
+
+    name_len /= sizeof(WCHAR);
+
+    uv__free(name_info);
+    uv__pipe_unpause_read((uv_pipe_t*)handle); /* cast away const warning */
   } else {
-    name_buf = name_info->FileName;
-    name_len = name_info->FileNameLength;
+    /* the pipe name is handle->name */
+    name_buf = handle->name;
+    name_size = wcslen(handle->name);
   }
-
-  if (name_len == 0) {
-    *size = 0;
-    err = 0;
-    goto error;
-  }
-
-  name_len /= sizeof(WCHAR);
 
   /* check how much space we need */
   addrlen = WideCharToMultiByte(CP_UTF8,
@@ -1989,13 +2002,11 @@ static int uv__pipe_getname(const uv_pipe_t* handle, char* buffer, size_t* size)
                                 NULL);
   if (!addrlen) {
     *size = 0;
-    err = uv_translate_sys_error(GetLastError());
-    goto error;
+    return uv_translate_sys_error(GetLastError());
   } else if (pipe_prefix_len + addrlen > *size) {
     /* "\\\\.\\pipe" + name */
     *size = pipe_prefix_len + addrlen;
-    err = UV_ENOBUFS;
-    goto error;
+    return UV_ENOBUFS;
   }
 
   memcpy(buffer, pipe_prefix, pipe_prefix_len);
@@ -2009,22 +2020,13 @@ static int uv__pipe_getname(const uv_pipe_t* handle, char* buffer, size_t* size)
                                 NULL);
   if (!addrlen) {
     *size = 0;
-    err = uv_translate_sys_error(GetLastError());
-    goto error;
+    return uv_translate_sys_error(GetLastError());
   }
 
   addrlen += pipe_prefix_len;
   *size = addrlen;
 
-  err = 0;
-  goto cleanup;
-
-error:
-  uv__free(name_info);
-
-cleanup:
-  uv__pipe_unpause_read((uv_pipe_t*)handle); /* cast away const warning */
-  return err;
+  return 0;
 }
 
 
